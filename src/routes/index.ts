7978c4e720436a34707b896449fc558ae305ed8d
Hector <hector.adjakpa@gmail.com>
2025-11-04 16:52:08 +0100

    nouvelle branche

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -23,12 +23,12 @@ router.use('/defenses', defenseRoutes);
 router.use('/grading', gradingRoutes);
 
 // Routes combinées pour certaines fonctionnalités
-router.use('/projects/:projectId/groups', groupRoutes);
-router.use('/projects/:projectId/reports', reportRoutes);
-router.use('/projects/defenses', defenseRoutes);
-router.use('/projects/:projectId/deliverables', deliverableRoutes);
-router.use('/projects/:projectId/defenses', defenseRoutes);
-router.use('/projects/:projectId/grading', gradingRoutes);
+// router.use('/projects/:projectId/groups', groupRoutes);
+// router.use('/projects/:projectId/reports', reportRoutes);
+// router.use('/projects/defenses', defenseRoutes);
+// router.use('/projects/:projectId/deliverables', deliverableRoutes);
+// router.use('/projects/:projectId/defenses', defenseRoutes);
+// router.use('/projects/:projectId/grading', gradingRoutes);
 
 router.use(buildAntiCheatRouter(AppDataSource));
 
